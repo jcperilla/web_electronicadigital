8235c891d94f90b8a3b44304db14cfd08d7a7dcd
jhonnysierra <jhonnysierrap@gmail.com>
2019-06-29 02:17:49 -0500

    Create util.js
    Se crea archivo json

---
+++ public/js/util.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	alert('hola');
+	setInterval(function(){ get_info_parking_lot(); }, 3000);
+});
+
+function get_info_parking_lot(){
+	$.ajax({
+		url: "http://localhost/web_electronicadigital/main/GET_query_parking",
+		success: function(respuesta) {
+			console.log(respuesta);
+		},
+		error: function(x) {
+	        console.log("No se ha podido obtener la información" + x);
+	    }
+	});
+}

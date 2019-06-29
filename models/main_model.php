76d026d2dc952153fcff0a7cf8ce791d80add3ce
jhonnysierra <jhonnysierrap@gmail.com>
2019-06-28 23:27:14 -0500

    Create main_model.php
    Se crea el modelo para las vistas de la carpeta main y se crea una funcion que realiza la consulta a la base de datos para traer la informacion del parqueadero que se mostrara en la interfaz

---
+++ models/main_model.php
@@ -0,0 +1,24 @@
+<?php 
+    class MainModel extends Model {
+
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function query_parking($data) {
+            $query = $this->db->connect()->prepare('SELECT * FROM parking_lot where id = :id');
+            $query->execute(['id' => $data['id']]);
+
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+
+            if(isset($row)) {
+               return ['available' => $row['available'], 'busy' => $row['busy']];
+            }
+            else { 
+               return null;
+            }
+            
+        }
+    }
+
+?>

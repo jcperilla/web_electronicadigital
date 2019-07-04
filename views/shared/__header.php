b765f0c8700cd9febf1e1835f3a26c81309b2a95
Sebastian Caro <sebascaro0806@gmail.com>
2019-07-04 00:48:55 -0500

    updated header

--- views/shared/__header.php
+++ views/shared/__header.php
@@ -17,23 +17,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light ">
   <a class="navbar-brand" href="#">Parqueadero</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <span class="fa fa-bars"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <?php 
-      if(!isset(Session::get('user')['id'])) { ?>
-        <li class="nav-item active">
-          <a class="nav-link" href="<?php echo constant('URL'); ?>account">Login <span class="sr-only">(current)</span></a>
-        </li>
-     <?php  }
-      else { ?>
-        <li class="nav-item active ">
-          <a class="nav-link" href="<?php echo constant('URL'); ?>account/destroySession">Cerrar sesion <span class="sr-only">(current)</span></a>
-        </li>
-      <?php }
-      ?>
       <li class="nav-item active">
         <a class="nav-link" href="<?php echo constant('URL'); ?>main/consulta">Inicio <span class="sr-only">(current)</span></a>
       </li>
@@ -52,7 +40,7 @@
     <?php 
         if(isset(Session::get('user')['id'])) {
       ?>
-      <div class="form-inline my-2 my-lg-0 ">
+      <div class="form-inline my-2 my-lg-0 "> 
       <ul class="navbar-nav mr-auto">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -60,17 +48,21 @@
             <?php  echo Session::get('user')['first_name'].' '.Session::get('user')['last_name']?>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="<?php echo constant('URL'); ?>account/destroySession">Cerrar sesion <span class="sr-only">(current)</span></a>
             </div>
           </li>
         </ul>
       </div>
       <?php }
-        
-      ?>
+      else { ?>
+      <div class="form-inline my-2 my-lg-0 "> 
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo constant('URL'); ?>account">Login <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+      <?php } ?>
   </div>
 </nav>
 </body>

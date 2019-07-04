8933e65930d45568a2997907f56ddc069998f07e
jhonnysierra <jhonnysierrap@gmail.com>
2019-07-03 23:57:45 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-    define('URL', 'http://localhost/parking_lot/');
+    define('URL', 'http://localhost/web_electronicadigital/');
 
     define('HOST', 'localhost');
-    define('DB', 'parkinglot');
+    define('DB', 'digital_project');
     define('USER', 'root');
     define('PASSWORD', '');
     define('CHARSET', 'utf8mb4');

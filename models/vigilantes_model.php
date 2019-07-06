88d2d142c720ee135c0237e52580783529c0acd2
jhonnysierra <jhonnysierrap@gmail.com>
2019-07-05 23:28:00 -0500

    Update vigilantes_model.php
    Actualizacion para que no se registren usuarios duplicador por email o id

--- models/vigilantes_model.php
+++ models/vigilantes_model.php
@@ -24,19 +24,69 @@
         }
 
         /**
+         * Funcion que verifica por id si existe un vigilante
+         */
+        public function query_exist_vigilant_id($data) {
+            $query = $this->db->connect()->prepare('SELECT * FROM user u WHERE u.type_user="2" AND u.id = :id');
+            $query->execute(['id' => $data['id']]);
+
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+
+            if(isset($row)) {
+               return $row;
+            }
+            else { 
+               return null;
+            }
+            
+        }        
+
+
+        /**
+         * Funcion que verifica por email si existe un vigilante
+         */
+        public function query_exist_vigilant_email($data) {
+            $query = $this->db->connect()->prepare('SELECT * FROM user u WHERE u.type_user="2" AND u.email = :email');
+            $query->execute(['email' => $data['email']]);
+
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+
+            if(isset($row)) {
+               return $row;
+            }
+            else { 
+               return null;
+            }
+            
+        } 
+        /**
          * Método para crear un vigilante
          */
         public function crearVigilante($data)
         {
-            //Se inserta en la tabla principal (user)
-            $query = $this->db->connect()->prepare('INSERT INTO user (id, first_name, last_name, email, password, type_user) VALUES (?, ?, ?, ?, ?, ?)');
-            $type_user='2';
-            $pass = password_hash($data["password"], PASSWORD_BCRYPT);
-            $query->execute([$data["id"], $data["first_name"], $data["last_name"], $data["email"], $pass ,$type_user]);
 
-            //Se inserta en la tabla vigilant
-            $query = $this->db->connect()->prepare('INSERT INTO vigilant (id, entry_time, departure_time) VALUES (?, ?, ?)');
-            $query->execute([$data["id"], $data["entry_time"], $data["departure_time"]]);
+            $result_exist_id = $this->query_exist_vigilant_id($data);
+            $result_exist_email = $this->query_exist_vigilant_email($data);
+
+            if (empty($result_exist_id) && empty($result_exist_email)) {
+
+
+                //Se inserta en la tabla principal (user)
+                $query = $this->db->connect()->prepare('INSERT INTO user (id, first_name, last_name, email, password, type_user) VALUES (?, ?, ?, ?, ?, ?)');
+                $type_user='2';
+                $pass = password_hash($data["password"], PASSWORD_BCRYPT);
+                $query->execute([$data["id"], $data["first_name"], $data["last_name"], $data["email"], $pass ,$type_user]);
+
+                //Se inserta en la tabla vigilant
+                $query = $this->db->connect()->prepare('INSERT INTO vigilant (id, entry_time, departure_time) VALUES (?, ?, ?)');
+                $query->execute([$data["id"], $data["entry_time"], $data["departure_time"]]);
+                
+                return true;
+            } else {
+                return null;
+            }
+            
+
         }
 
         /**
@@ -48,8 +98,9 @@
             $query = $this->db->connect()->prepare('UPDATE user SET  first_name = ?, last_name = ?, email = ?, password = ?, type_user = ? WHERE id =  ?');
             
             $type_user='2';
+            $pass = password_hash($data["password"], PASSWORD_BCRYPT);
 
-            $query->execute([$data["first_name"], $data["last_name"], $data["email"], $data["password"], $type_user, $data["id"]]);
+            $query->execute([$data["first_name"], $data["last_name"], $data["email"], $pass, $type_user, $data["id"]]);
 
             //Se actualiza el horario del vigilante
             $query2 = $this->db->connect()->prepare('UPDATE vigilant SET entry_time = ?, departure_time = ? WHERE id =  ?');
